305a650928ec03b396b5a8810dad9513346644bb
ManuelMorenoCámara <manuel.moreno@babelgroup.com>
2022-06-16 08:16:25 +0200

    Launch, Splash, and Logging Screen creation

---
+++ ProyectoMobile/Scenes/LogIn/LogInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  ProyectoMobile
+//
+//  Created by manuel.moreno.local on 13/6/22.
+//
+
+import Foundation
---
+++ ProyectoMobile/Scenes/Utils/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  ProyectoMobile
+//
+//  Created by manuel.moreno.local on 16/6/22.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    struct Colors {
+        static let loginButtonTextColor: UIColor? = UIColor(named: "LoginButtonTextColor")
+        static let textFieldAndButtonsColor: UIColor? = UIColor(named: "TextFieldAndButtonsColor")
+    }
+
+    struct LoggingTexts {
+        static let appName: String = "Arbolum"
+        static let logIn: String = "Identifícate"
+        static let haveAccount: String = "¿No tienes cuenta?"
+        static let register: String = "Regístrate"
+    }
+}
